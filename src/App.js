7f27047cb1396b0382e722f3176ad274df55f14c
Lars Nolden <larsnolden@web.de>
2018-01-24 10:15:54 -0500

    interior and poi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import 'aframe';
 
+import interior from './assets/gmc_equirectangular.png'
+
 class App extends Component {
   render() {
     return (
@@ -9,14 +11,17 @@ class App extends Component {
         <a-scene>
           <a-assets>
             <img alt="ground" id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
-            <img alt="sky" id="skyTexture" src="https://c1.staticflickr.com/5/4697/24938049577_5c932d583b_f.jpg"/>
+            <img alt="sky" id="skyTexture" src={interior}/>
           </a-assets>
-          <a-box color="red" position="0 2 4" rotation="0 0 10"></a-box>
-          <a-light type="ambient" color="#aba"></a-light>
-          <a-camera>
-            <a-cursor></a-cursor>
-        </a-camera>
-        <a-sky src="#skyTexture"></a-sky>
+          <a-sphere color="#fff" position="-2 1 0" radius="0.08"/>
+          <a-sphere color="#fff" position="-2 1 -2" radius="0.08"/>
+          <a-light type="ambient" color="#fff"></a-light>
+          <a-entity rotation="0 90 0">
+            <a-camera>
+              <a-cursor></a-cursor>
+            </a-camera>
+          </a-entity>
+          <a-sky src="#skyTexture"></a-sky>
         </a-scene>
       </div>
     );
